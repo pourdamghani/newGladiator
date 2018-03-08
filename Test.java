e8e2b11d03ba99fe73bd99c827bd5e679ec24241
pourdamghani <pourdamghani@gmail.com>
2018-03-08 14:21:33 +0330

    Root of Tree completed

--- Test.java
+++ Test.java
@@ -5,29 +5,22 @@ import java.util.Scanner;
  */
 
 class Player{
-    private Integer number_of_troops, power_of_troops;
     private Integer alive_power,alive_troops;
     private Integer id;
 
     Player(Integer id){
-        Scanner playerScanner = new Scanner(System.in);
-        number_of_troops = playerScanner.nextInt();
-        power_of_troops = playerScanner.nextInt();
         this.id = id;
+        Scanner playerScanner = new Scanner(System.in);
+        System.out.println("Please enter number of troops of player " + id + ":");
+        alive_troops = playerScanner.nextInt();
+        System.out.println("Please power of troops of player " + id + ":");
+        alive_power = playerScanner.nextInt();
     }
 
-    /*public Integer getNumber() {
-        return number_of_troops;
-    }
+    public Integer getAlive_power() { return alive_power; }
+    public Integer getAlive_troops() { return alive_troops; }
 
-    public Integer getPower() {
-        return power_of_troops;
-    }*/
-
-    public Integer getAlive_power() {
-        return alive_power;
-    }
-    public void decrese(Integer amount){
+    public void decrease(Integer amount){
         alive_power = alive_power-amount;
         alive_troops = alive_troops-1;
     }
@@ -36,12 +29,9 @@ class Player{
         alive_troops = alive_troops+1;
 
     }
-    public boolean empty(){
-        return (alive_power == 0 || alive_troops == 0);
-    }
-    public boolean isFirstPlayer(){
-        return id == 1;
-    }
+
+    public boolean empty(){ return (alive_power == 0 || alive_troops == 0); }
+    public boolean isFirstPlayer(){ return id == 1; }
 }
 
 class Fraction{
@@ -57,16 +47,8 @@ class Fraction{
         this.denominator = denominator;
     }
 
-    private BigInteger getNumerator() {
-        return numerator;
-    }
-
-    private BigInteger getDenominator() {
-        return denominator;
-    }
-
     public Fraction multi(Fraction second){
-        return new Fraction(getNumerator().multiply(second.getNumerator()),getDenominator().multiply(second.getDenominator()));
+        return new Fraction(numerator.multiply(second.numerator),denominator.multiply(second.denominator));
     }
     public Fraction plus(Fraction other){
         BigInteger lcm = Fraction.LCM(denominator,other.denominator);
@@ -96,14 +78,24 @@ class Fraction{
 
 public class Test {
     public static void main(String[] args) {
-        solveForTwoPlayers(new Player(1), new Player(2), null);
+        Player firstPlyaer = new Player(1), secondPlayer = new Player(2);
+        Fraction[][] matrix = new  Fraction[firstPlyaer.getAlive_troops()][secondPlayer.getAlive_troops()];
+        System.out.println("Thank you:");
+        for (Integer first_power = 1; first_power <= firstPlyaer.getAlive_power(); first_power++) {
+            firstPlyaer.decrease(first_power);
+            for (Integer second_power = 1; second_power <= secondPlayer.getAlive_power(); second_power++) {
+                secondPlayer.decrease(second_power);
+                Fraction first_wins = solveForTwoPlayers(secondPlayer,firstPlyaer,first_power);
+                Fraction second_wins = solveForTwoPlayers(firstPlyaer,secondPlayer,second_power);
+                matrix[first_power][second_power] = first_wins.plus(second_wins);
+                secondPlayer.increase(second_power);
+            }
+            firstPlyaer.increase(first_power);
+        }
+        System.out.println("First Phase Finished");
     }
 
     private static Fraction solveForTwoPlayers(Player looser,Player winner, Integer winnerPower){
-        if (winnerPower == null) {
-            return null;
-        }
-        else{
         if(looser.empty()){
             if(looser.isFirstPlayer())
                 return new Fraction(-1,2);
@@ -115,9 +107,9 @@ public class Test {
             wholeTemp = new Fraction(0,1);
         else
             wholeTemp = new Fraction(1,1);
-        for (Integer power = 0; power < looser.getAlive_power(); power++) {
+        for (Integer power = 1; power <= looser.getAlive_power(); power++) {
             Integer wholePower = winnerPower+power;
-            looser.decrese(power);
+            looser.decrease(power);
             Fraction countinue = solveForTwoPlayers(looser,winner,winnerPower).multi(new Fraction(winnerPower,wholePower));
             Fraction changed = solveForTwoPlayers(winner,looser,power).multi(new Fraction(power,wholePower));
             Fraction temp = changed.plus(countinue);
@@ -128,7 +120,6 @@ public class Test {
                 wholeTemp = Fraction.min(temp,wholeTemp);
             }
             return wholeTemp;
-        }
     }
 }
 
